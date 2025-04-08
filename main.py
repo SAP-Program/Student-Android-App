f75595b1c751450802e60b523be93354807e5250
parsasafaie <parsasafaie.2568@proton.me>
2025-04-08 19:18:08 +0330

    use database login functions in main and build primary home page

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from kivymd.app import MDApp
 from kivy.metrics import dp
+from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
+from database_handler import check_identity
 
 KV = '''
-MDScreen:
-    md_bg_color: 0.1, 0.1, 0.1, 1
+ScreenManager:
+    LoginScreen:
+        name: "login"
+    HomeScreen:
+        name: "home"
+
+<LoginScreen>:
 
     BoxLayout:
         orientation: "vertical"
@@ -43,12 +50,44 @@ MDScreen:
             md_bg_color: 0.2, 0.6, 1, 1
             text_color: 1, 1, 1, 1
             size_hint_x: 1
+
+<HomeScreen>:
+    md_bg_color: 0.2, 0.2, 0.2, 1
+
+    MDLabel:
+        text: "Welcome!"
+        halign: "center"
+        pos_hint: {"center_y": 0.85}
+        font_style: "H4"
+        theme_text_color: "Custom"
+        text_color: 1, 1, 1, 1
+
+    MDRaisedButton:
+        text: "Start"
+        pos_hint: {"center_x": 0.5, "center_y": 0.4}
+        text_color: 1, 1, 1, 1
+        md_bg_color: 0.2, 0.6, 1, 1
+
 '''
 
-class LoginApp(MDApp):
+class LoginScreen(MDScreen):
+    pass
+
+class HomeScreen(MDScreen):
+    pass
+
+class Main(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Blue"
         return Builder.load_string(KV)
+    
+    def login(self):
+        login_screen = self.root.get_screen("login")
+        nc = login_screen.ids.nc_field.text
+        password = login_screen.ids.password_field.text
 
-LoginApp().run()
+        if check_identity(nc=nc, password=password):
+            self.root.current = 'home'
+
+Main().run()
